c546005eb750ed019ba2d2ecac9a386bc41f22eb
Thanh0201 <thanhho@metropolia.fi>
2025-11-25 09:47:27 +0200

    [iter3] Understand Authentication Logic-Thanh

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -9,16 +9,18 @@ const requireAuth = async (req, res, next) => {
     return res.status(401).json({ error: "Authorization token required" });
   }
 
-//   console.log(authorization);
-//   console.log(authorization.split(" "));
-//   console.log(authorization.split(" ")[0]);
-//   console.log(authorization.split(" ")[1]);
+   console.log(authorization);
+   console.log(authorization.split(" "));
+   console.log(authorization.split(" ")[0]);
+   console.log(authorization.split(" ")[1]);
 
   const token = authorization.split(" ")[1];
 
   try {
     const { _id } = jwt.verify(token, process.env.SECRET);
-
+    // Finds the User document whose _id matches the decoded token's _id.
+    //.select("_id") ensures only the _id field is returned (no password, email, etc.).
+    //The found document is then assigned to req.user. Downstream controllers can use req.user._id to scope queries.
     req.user = await User.findOne({ _id }).select("_id");
     next();
   } catch (error) {
